967b083b5542af091f754aaa8336b8b43fb3b91d
Jeganathan Swaminathan <mail2jegan@gmail.com>
2024-05-16 15:39:58 +0530

    Added Day3 lab exercises.

---
+++ Day3/hello-microservice/src/main/java/org/tektutor/HelloController.java
@@ -0,0 +1,46 @@
+package org.tektutor;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.*;
+import java.sql.*;
+
+@RestController
+public class HelloController {
+
+	public String readGreetingMsgFromDB() {
+		//String url = "jdbc:mysql://localhost:3306/tektutor";
+		String url = System.getenv("jdbc-url");
+		String username = System.getenv("username");
+		String password = System.getenv("password");
+
+		String query = "select * from greeting";
+
+		String msg = "";
+
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection connection = DriverManager.getConnection(url,username,password);
+			Statement statement   = connection.createStatement();
+			ResultSet resultSet   = statement.executeQuery(query);
+			resultSet.next();
+
+			msg = resultSet.getString("message");
+
+			statement.close();
+			connection.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		return msg; 
+	}
+
+	@RequestMapping("/")
+	public String sayHello() {
+		return readGreetingMsgFromDB();
+	}
+}
