dc5d7511e23e6d6e1427c1d9d985507583b58c9b
Jeganathan Swaminathan <mail2jegan@gmail.com>
2024-05-16 12:10:04 +0530

    Added Day3 lab exercises.

---
+++ Day3/persistent-volume/wordpress/deploy.sh
@@ -0,0 +1,15 @@
+echo "\nDeploying mariadb sever ..."
+oc apply -f mariadb-pv.yml
+oc apply -f mariadb-pvc.yml
+oc apply -f mariadb-deploy.yml
+oc apply -f mariadb-svc.yml
+
+
+echo "\nDeploying wordpress server ..."
+oc apply -f wordpress-pv.yml
+oc apply -f wordpress-pvc.yml
+oc apply -f wordpress-deploy.yml
+oc apply -f wordpress-svc.yml
+oc apply -f wordpress-route.yml
+
+oc get all
